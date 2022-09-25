9e56641e49be1ca4042265e5fd5334cc9395ba59
Cesar Lisarazo <cesarlisarazo@gmail.com>
2022-09-25 18:05:20 -0300

    Java

---
+++ JS/script.js
@@ -0,0 +1,57 @@
+/*
+let numero = prompt("Ingrese un numero:")
+
+if(numero>5)
+{
+console.log("Se ejecuto la opercion")
+
+}
+
+
+let texto = prompt("Ingrese SI para comprar \nIngrese NO para salir")
+if (texto=="SI")
+{
+    alert("Gracias por su respuesta")
+}
+else
+{
+    alert("Gracias vuelva prontos!")
+}
+
+
+let precio = prompt("Ingrese un numero:")
+
+if (precio<20)
+{
+    alert("El precio es menor que 20");
+}
+
+else if(precio<50)
+{
+    alert("El precio es menor que 50")
+}
+
+else if(precio<100)
+{
+    alert("El precio es menor que 100")
+}
+
+else
+{
+    alert("El precio es mayor a 100")
+}
+
+console.log("Fin")
+
+
+
+//condional de conteo
+
+for(let i=1; i<=10; i=i+1)
+{
+    alert("El número es " + i)
+}
+
+*/
+
+ 
